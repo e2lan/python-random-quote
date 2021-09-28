a6a140de379c7ef58416860481a2aeb529c8cdde
e2lan <91542812+e2lan@users.noreply.github.com>
2021-09-29 01:49:43 +0300

    My implementation few quotes

--- get-quote.py
+++ get-quote.py
@@ -6,10 +6,14 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
-  rnd = mkl_random.randint(0,last)
-  
-  print(quotes[rnd])
+  last = len(quotes) - 1
+  num= int(input("how many quotes do you want to print?"))
+  while num>last :
+      num = int(input("how many quotes do you want to print?"))
+      
+  for i in range(num):
+      rnd = mkl_random.randint(0,last) 
+      print(quotes[rnd], end=" ")
 
 if __name__== "__main__":
   primary()
